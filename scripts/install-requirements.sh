67dc1fce068cc4ec6af50da07f59cb6284939421
Simon Letort <simon.letort@digitalasset.com>
2025-02-13 23:25:23 +0000

    install script

---
+++ scripts/install-requirements.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# install nix
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+. /home/codespace/.nix-profile/etc/profile.d/nix.sh
+
+# install direnv
+sudo apt update
+sudo apt install direnv
+# allow direnv
+direnv allow
+
+# install java 17
+# this didn't work: sudo apt update && sudo apt install openjdk-17-jdk -y
+nix-env -iA nixpkgs.openjdk17
+
+echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+java -version
+
+
+cd quickstart
+make install-daml-sdk
+
